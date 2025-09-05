1eed751133434910e337f0e9b9a32c7023167606
Diego Santana <3c.santana@gmail.com>
2025-09-05 20:54:19 -0300

    feat: Estrutura do exercício 05 (Módulos)

---
+++ ex05/main.js
@@ -0,0 +1,9 @@
+// main.js
+// 4. Importe a função `somar` e `PI` do módulo modulo-matematica.js (named imports)
+// Seu código aqui
+
+// 5. Importe a função default `subtrair` com um nome customizado (ex: `sub`)
+// Seu código aqui
+
+// 6. Use as funções importadas para calcular: (somar(PI, 2) + sub(5, 1))
+// Seu código aqui
---
+++ ex05/modulo-matematica.js
@@ -0,0 +1,9 @@
+// modulo-matematica.js
+// 1. Exporte uma função `somar` que recebe 2 números e retorna a soma
+// Seu código aqui
+
+// 2. Exporte uma constante `PI` com valor 3.1415 como named export
+// Seu código aqui
+
+// 3. Exporte default uma função `subtrair` (recebe 2 números, retorna a subtração)
+// Seu código aqui
