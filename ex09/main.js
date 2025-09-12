da51e40182815f27996d6bed69a7c4bf4b764df7
Diego Santana <3c.santana@gmail.com>
2025-09-12 19:01:17 -0300

    feat: Exercício 09

---
+++ ex09/main.js
@@ -0,0 +1,16 @@
+// Objetivo: Praticar indexOf(), startsWith(), endsWith() e includes().
+// Complete os desafios abaixo:
+
+const texto = "Aprender JavaScript abre portas.";
+
+// 1. Encontre a posição da palavra "JavaScript"
+console.log("Posição de 'JavaScript':",  texto.indexOf('JavaScript'));
+
+// 2. Verifique se o texto começa com "Aprender"
+console.log("Começa com 'Aprender'?", texto.startsWith('Aprender'));
+
+// 3. Verifique se o texto termina com "portas."
+console.log("Termina com 'portas.'?", texto.endsWith('portas.'));
+
+// 4. Verifique se "abre" existe no texto
+console.log("Contém 'abre'?", texto.includes('abre'));
