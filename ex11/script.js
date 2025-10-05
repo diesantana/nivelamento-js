5c411580ba30a89db8071a20d52a101200f1a199
Diego Santana <3c.santana@gmail.com>
2025-10-05 09:02:52 -0300

    Estrutura do exercício 11

---
+++ ex11/script.js
@@ -0,0 +1,16 @@
+// Objetivo: Praticar loops while e do...while.
+// Complete os desafios abaixo:
+
+// 1. Conte de 10 a 1 (use while)
+let contador = 10;
+// Seu código aqui
+
+// 2. Simule um login (peça senha até digitar "1234")
+let senha = "";
+// Seu código aqui (use prompt() ou simule no console)
+
+// 3. Use do...while para imprimir números ímpares < 20
+let num = 1;
+do {
+  // Seu código aqui
+} while (num < 20);
